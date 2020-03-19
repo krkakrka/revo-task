cff3ac5ca3795ca5c81caa52da16c5dff2929de5
Karolis Lesevicius <karolis.lesevicius@gmail.com>
2020-03-19 17:32:12 +0200

    Extract root reducer and add tests for EXCHANGE

---
+++ src/store/root.reducer.spec.js
@@ -0,0 +1,27 @@
+import { rootReducer } from './root.reducer';
+import { INITIAL_STATE } from '../constants';
+
+describe('exchangeReducer', () => {
+  let state;
+
+  beforeEach(() => {
+    state = {
+      ...INITIAL_STATE,
+      balances: {
+        EUR: 1,
+        USD: 0
+      },
+      exchange: {
+        from: { currency: 'EUR', value: 1 },
+        to: { currency: 'USD', value: 2 }
+      }
+    }
+  });
+
+  it('should update balances of exchange', () => {
+    const action = { type: 'EXCHANGE' };
+    const nextState = rootReducer(state, action);
+    expect(nextState.balances.EUR).toBe(0);
+    expect(nextState.balances.USD).toBe(2);
+  });
+});
---
+++ src/store/root.reducer.ts
@@ -0,0 +1,28 @@
+import { INITIAL_STATE } from '../constants';
+import { exchangeReducer } from './exchange.reducer';
+
+export function rootReducer(state = INITIAL_STATE, action) {
+  switch(action.type) {
+    case 'RATES_UPDATE':
+      return {
+        ...state,
+        rates: action.payload.rates
+      };
+    case 'EXCHANGE': {
+      return {
+        ...state,
+        balances: {
+          ...state.balances,
+          [state.exchange.from.currency]: state.balances[state.exchange.from.currency] - state.exchange.from.value,
+          [state.exchange.to.currency]: state.balances[state.exchange.to.currency] + state.exchange.to.value
+        },
+        exchange: exchangeReducer(state.exchange, state.rates, action)
+      };
+    }
+    default:
+      return {
+        ...state,
+        exchange: exchangeReducer(state.exchange, state.rates, action)
+      };
+  }
+}
